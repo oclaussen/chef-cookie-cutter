84abf2fd728c499cb32e810cb6a10be5fa3d8fe7
Ole Claussen <claussen.ole@gmail.com>
2016-02-28 15:00:34 +0100

    yard doc for run_state

--- lib/chef/cookie_cutter/run_state/recipe_dsl.rb
+++ lib/chef/cookie_cutter/run_state/recipe_dsl.rb
@@ -30,8 +30,17 @@ EOH
         end
       end
 
-      # @!visibility private
+      ##
+      # Extensions to the Chef recipe DSL.
+      #
       module RecipeDSL
+        ##
+        # Store a value in the run state.
+        #
+        # @overload store_state(*keys, value)
+        #   @param keys [Array<String, Symbol>] the key with which the value is stored
+        #   @param value the object to store
+        #
         def store_state(*subkeys, key, value)
           subkeys.map!(&:to_s)
           hash = node.run_state
@@ -42,6 +51,13 @@ EOH
           hash[key.to_s] = value
         end
 
+        ##
+        # Retrieve a value from the run state.
+        #
+        # @param keys [Array<String, Symbol>] the key with which the value is stored
+        # @return the value stored under the key
+        # @raise [RunStateDoesNotExistError] if no value exists with the key
+        #
         def fetch_state(*keys)
           keys.map!(&:to_s)
           keys.inject(node.run_state) do |hash, key|
@@ -50,6 +66,12 @@ EOH
           end
         end
 
+        ##
+        # Check if a value is stored in the run state.
+        #
+        # @param keys [Array<String, Symbol>] The key with which the value is stored
+        # @return [TrueClass, FalseClass] true iff a value exists with the key
+        #
         def exist_state?(*keys)
           fetch_state(*keys)
           true
