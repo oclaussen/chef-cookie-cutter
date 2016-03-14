853e117fa78f4f6ab65294eacfe7c63d8fde7e3c
Ole Claussen <claussen.ole@gmail.com>
2016-03-14 15:23:00 +0100

    add functions to describe property defaults and arguments better

--- lib/chef/cookie_cutter/autodocs/property_dsl.rb
+++ lib/chef/cookie_cutter/autodocs/property_dsl.rb
@@ -26,9 +26,8 @@ class Chef
         ##
         # Get the description set for property. A description on the resource
         # can be set initially by adding the new `:description` validation
-        # option. Will automatically add a description for `:default`,
-        # `:collect` and `:coerce_resource` validation options set on the
-        # property.
+        # option. Will automatically add a description for  `:collect` and
+        # `:coerce_resource` validation options set on the property.
         #
         # @return [String] the description for the resource
         #
@@ -38,12 +37,69 @@ class Chef
           desc += '.' unless desc.empty? || desc.end_with?('.')
           desc += " Must be a `#{options[:coerce_resource]}` resource or a block." if options.key?(:coerce_resource)
           desc += ' This attribute can be specified multiple times.' if options[:collect]
-          if options.key?(:default)
-            default = "`#{options[:default]}`"
-            default = options[:default].description if options[:default].is_a?(Chef::DelayedEvaluator)
-            desc += " Defaults to #{default}."
-          end
           desc
+        end
+
+        ##
+        # Get a description of the default value of the property. Will describe
+        # whether the property is required, name attribute or has a default
+        # value.
+        #
+        # @return [String] a description of the default value
+        #
+        def describe_default
+          if required?
+            '*Required*'
+          elsif name_property?
+            'The name of the resource'
+          elsif has_default?
+            if options[:default].is_a?(Chef::DelayedEvaluator)
+              options[:default].description
+            else
+              "`#{options[:default]}`"
+            end
+          else
+            ''
+          end
+        end
+
+        ##
+        # If the coercion of the property allows for multiple or named arguments
+        # (e.g. via the `fancy_property` module), get a textual description of
+        # these arguments
+        #
+        # @return [String] a description of the property coercion arguments
+        #
+        def describe_arguments
+          return '' unless respond_to?(:allow_kwargs?) && allow_kwargs?
+          method = if options.key?(:coerce_class)
+                     if options[:coerce_class].is_a?(Proc)
+                       target = Class.new
+                       target.class_eval(&options[:coerce_class])
+                     else
+                       target = options[:coerce_class]
+                     end
+                     target.instance_method(:initialize)
+                   elsif options.key?(:coerce)
+                     options[:coerce]
+                   end
+          params = method.parameters.map do |type, name|
+            # FIXME: does not consider required key arguments.
+            # FIXME: displays default values only as ?
+            case type
+            when :req
+              name.to_s
+            when :opt
+              "#{name} = ?"
+            when :rest
+              "*#{name}"
+            when :key
+              "#{name}: ?"
+            when :keyrest
+              "**#{name}"
+            end
+          end
+          "`#{params.join(', ')}`"
         end
 
         # @!visibility private
