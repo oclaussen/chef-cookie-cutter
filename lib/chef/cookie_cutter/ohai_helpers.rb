f00cc45e27775393c5c69a0f9013cbb36cb51d7b
Ole Claussen <claussen.ole@gmail.com>
2016-04-12 22:21:11 +0200

    added some nice ohai platform helpers

--- lib/chef/cookie_cutter/ohai_helpers.rb
+++ lib/chef/cookie_cutter/ohai_helpers.rb
@@ -16,25 +16,16 @@
 # limitations under the License.
 #
 
-##
-# The top level Chef class.
-#
+require 'chef/node'
+require 'chef/recipe'
+
 class Chef
-  ##
-  # Cookie Cutter is a collection of hacks and monkey patches for Chef.
-  #
   module CookieCutter
-    require 'chef/cookie_cutter/version'
+    module OhaiHelpers
+      require 'chef/cookie_cutter/ohai_helpers/node_dsl'
 
-    require 'chef/cookie_cutter/collect_property'
-    require 'chef/cookie_cutter/fancy_property'
-    require 'chef/cookie_cutter/include_resource'
-    require 'chef/cookie_cutter/ohai_helpers'
-    require 'chef/cookie_cutter/provides_named'
-    require 'chef/cookie_cutter/run_state'
-    require 'chef/cookie_cutter/shared_properties'
-
-    require 'chef/cookie_cutter/autodocs'
-    require 'chef/cookie_cutter/spec_matchers'
+      ::Chef::Node.send :include, NodeDSL
+      ::Chef::Recipe.send :include, NodeDSL
+    end
   end
 end
---
+++ lib/chef/cookie_cutter/ohai_helpers/node_dsl.rb
@@ -0,0 +1,96 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+#
+# Copyright 2016, Ole Claussen <claussen.ole@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+class Chef
+  module CookieCutter
+    module OhaiHelpers
+      module NodeDSL
+        %w(
+          aix
+          amazon
+          arch
+          centos
+          debian
+          fedora
+          freebsd
+          gentoo
+          ios_xr
+          linuxmint
+          mac_os_x
+          nexus
+          omnios
+          openbsd
+          oracle
+          raspbian
+          redhat
+          rhel
+          scientific
+          slackware
+          smartos
+          solaris2
+          suse
+          ubuntu
+          windows
+          wrlinux
+        ).each do |name|
+          define_method "#{name}?".to_sym do
+            [
+              node['platform_family'] == name,
+              node['platform'] == name
+            ].any?
+          end
+        end
+
+        %w(
+          azure
+          cloud
+          cloudstack
+          digital_ocean
+          ec2
+          eucalyptus
+          gce
+          linode
+          openstack
+          rackspace
+          vagrant
+        ).each do |name|
+          define_method "#{name}?".to_sym do
+            node.key?(name)
+          end
+        end
+
+        %w(
+          kvm
+          lxc
+          openvz
+          vbox
+          vmware
+        ).each do |name|
+          define_method "#{name}?".to_sym do
+            return false unless node.key?('virtualization')
+            node['virtualization']['system'] == name
+          end
+        end
+
+        def docker?
+          ::File.exist?('/.dockerinit') || ::File.exist?('/.dockerenv')
+        end
+      end
+    end
+  end
+end
