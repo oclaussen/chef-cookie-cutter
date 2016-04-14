36337bbc38963c9cbe5d4226dcd6a591770a4ae8
Ole Claussen <claussen.ole@gmail.com>
2016-04-14 23:09:14 +0200

    added user helpers

---
+++ lib/chef/cookie_cutter/user_helpers/recipe_dsl.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+#
+# Copyright 2016, Ole Claussen <claussen.ole@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'etc'
+
+class Chef
+  module CookieCutter
+    module UserHelpers
+      module RecipeDSL
+        def home_directory(user)
+          unless user.nil?
+            Etc.endpwent
+            home = begin
+                     Dir.home(user)
+                   rescue ArgumentError
+                     nil
+                   end
+            return home unless home.nil?
+          end
+          return ENV.fetch('SystemRoot', 'C:\\') if node['platform_family'] == 'windows'
+          '/'
+        end
+      end
+    end
+  end
+end
